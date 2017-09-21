1b9b5265f7b91df66434d17f75210fbd09e132ec
lunzhang <lun.zhang1993@gmail.com>
2017-09-21 12:10:30 -0400

    added instructions

--- src/containers/main.js
+++ src/containers/main.js
@@ -23,6 +23,26 @@ class Main extends Component {
   render() {
     return (
       <div id="main">
+        <div id="instructions">
+          <h1>
+            Instructions
+          </h1>
+          <p>
+            1. Click on the tiles to add letters.
+          </p>
+          <p>
+            2. Set the index under each tile to reflect must use letters.
+            <b> Exe: </b> -1 means the letter {"doesn't"} have to appear in the word.
+            0 means the letter can be anywhere in the word.
+            1 means the letter has to be the first letter in the word, etc...
+          </p>
+          <p>
+            3. Click on Generate to get a list of available words from the letters, ordered based on descending point value.
+          </p>
+          <p>
+            4. Click on Clear to remove all the selected letters and reset all indexes on letters.
+          </p>
+        </div>
         <AvailableLetters ref="availableLetters" letterValues={this.props.letterValues} generateWords={this.generateWords} clearLetters={this.clearLetters} />
         <AllLetters ref="allLetters" letters={this.props.letters} letterValues={this.props.letterValues} addLetter={this.addLetter} />
         <Words ref="words" />
